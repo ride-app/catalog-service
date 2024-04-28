7f4c97a9100730c65f45d54796f95c2b3f33797e
Debkanchan Samadder <debu.samadder@gmail.com>
2024-04-28 22:56:10 +0530

    build: fix invalid deps reference

--- internal/api-handlers/get_route.go
+++ internal/api-handlers/get_route.go
@@ -5,11 +5,34 @@ import (
 	"errors"
 
 	"connectrpc.com/connect"
+	"github.com/bufbuild/protovalidate-go"
 	pb "github.com/ride-app/catalog-service/api/ride/catalog/v1alpha1"
 )
 
 func (service *CatalogServiceServer) GetRoute(ctx context.Context,
 	req *connect.Request[pb.GetRouteRequest],
 ) (*connect.Response[pb.GetRouteResponse], error) {
+	log := service.logger.WithFields(map[string]string{
+		"method": "GetRoute",
+	})
+
+	validator, err := protovalidate.New()
+	if err != nil {
+		log.WithError(err).Info("Failed to initialize validator")
+
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	if err := validator.Validate(req.Msg); err != nil {
+		log.WithError(err).Info("Invalid request")
+
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+
+	// switch x := req.Msg.Origin.(type) {
+	// case *pb.GetRouteRequest_Location_Place:
+
+	// }
+
 	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("unimplemented"))
 }
